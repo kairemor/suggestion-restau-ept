5641f54ef42f45981d7995a2d3c34f14e2427709
kmtc44 <kairmor@yahoo.com>
2020-02-07 22:48:47 +0000

    config utl auth service

--- src/config.js
+++ src/config.js
@@ -1,2 +1,3 @@
-export const auth_url = 'http://auth-express-dev.us-west-2.elasticbeanstalk.com';
+// export const auth_url = 'https://auth-express-dev.us-west-2.elasticbeanstalk.com';
+export const auth_url = 'https://auth-express-km.herokuapp.com/';
 export const sugges_url = 'https://linen-fuze-261912.appspot.com/repas';
