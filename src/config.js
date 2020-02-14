786157b1bd446a7dda104d8b23472cb3e73b5559
kmtc44 <kairmor@yahoo.com>
2020-02-14 09:02:16 +0000

    ackend url

--- src/config.js
+++ src/config.js
@@ -1,3 +1,4 @@
 // export const auth_url = 'https://auth-express-dev.us-west-2.elasticbeanstalk.com';
 export const auth_url = 'https://auth-express-km.herokuapp.com';
-export const sugges_url = 'https://linen-fuze-261912.appspot.com';
+//export const sugges_url = 'https://linen-fuze-261912.appspot.com';
+export const sugges_url = 'https://repas-sugges.herokuapp.com';
